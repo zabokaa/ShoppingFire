c9bbd8d805d738acf3b104991c97b75222a7b510
zabokaa <>
2023-07-18 19:21:32 +0200

    Not working -- problmes with version dependencies

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, View } from 'react-native';
 // create navigator, IM before navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
